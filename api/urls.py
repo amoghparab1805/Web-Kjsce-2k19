d2dbef7e1efbec8f3458b34a24b6e75fa229994f
Amogh Parab <amoghparab247@gmail.com>
2019-10-04 21:01:12 +0530

    changes to mail view

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,4 @@ from .views import *
 
 urlpatterns = [
     path('sign-up/', SignUp.as_view(), name='signup'),
-    path('send-mail-or-sms/', SendUserEmailOrSMS.as_view(), name='send_mail_or_sms'),
 ]
